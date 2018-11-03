69a1dead2b066679db938e8b8787f13fbf56646a
PradiptaSaha999 <pradiptasaha999@gmail.com>
2018-11-03 16:45:14 +0600

    day1 3/11/18 . User management Without confirmation maill

---
+++ MvcAuth/MvcAuth/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAuth.Controllers
+{
+    //mvcauth-221322
+    //Google Clint ID: 841815069120-tj01b1it6qr65taj08t24662rqjvnh04.apps.googleusercontent.com
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
